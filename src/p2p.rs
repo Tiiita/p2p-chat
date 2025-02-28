f6e27652e29d19829f0bea439347b6e2a7e7acbd
Tiiita <96084154+Tiiita@users.noreply.github.com>
2025-02-28 18:04:08 +0100

    fix automatically closed stream

--- src/p2p.rs
+++ src/p2p.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, Read, Write}, net::{TcpListener, TcpStream}};
+use std::{io::{self, Read, Write}, net::{TcpListener, TcpStream}, thread};
 
 use colored::Colorize;
 
@@ -17,10 +17,12 @@ impl P2P {
         let mut buf = msg.as_bytes();
         if let Err(why) = self.stream.write_all(&mut buf) {
             log!("ERROR: Failed to write message: {}", why);
+            return;
         }
 
         if let Err(why) = self.stream.flush() {
             log!("ERROR: Failed to flush written tcp stream: {}", why);
+            return;
         }
 
         log!("Sent message: {}", msg.bright_green());
@@ -33,19 +35,36 @@ pub fn listen(target: &str) -> std::io::Result<()> {
 
     for stream in listener.incoming() {
         let stream = stream?;
-        if let Err(e) = handle_connection(stream) {
-            log!("ERROR: Failed to handle connection: {}", e);
-        }
+        thread::spawn(move || {
+            if let Err(e) = handle_connection(stream) {
+                log!("ERROR: Failed to handle connection: {}", e);
+            }
+        });
     }
     
     Ok(())
 }
 
+
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buf = [0u8; 1024];
-    let bytes_read = stream.read(&mut buf)?;
 
-    let msg = String::from_utf8_lossy(&buf[..bytes_read]);
-    log!("Got message: {}", msg.bright_green());
+    loop {
+        let bytes_read = match stream.read(&mut buf) {
+            Ok(0) => {
+                log!("Connection closed by target peer");
+                break;
+            }
+            Ok(n) => n,
+            Err(e) => {
+                log!("ERROR: Failed to read from stream: {}", e);
+                break;
+            }
+        };
+
+        let msg = String::from_utf8_lossy(&buf[..bytes_read]);
+        log!("Got message: {}", msg.bright_green());
+    }
+
     Ok(())
 }
